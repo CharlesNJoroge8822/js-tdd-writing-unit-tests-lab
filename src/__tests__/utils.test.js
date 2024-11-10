153eb2f118e5afa2116a5584a83a3dfd57a77c63
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-10 15:17:58 +0300

    changes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,26 @@
-// Your tests here
+import isPalindrome from "../utils";
+
+describe ('isPalindrome', () => {
+
+    it ("return true for 'racecar' ", () => {
+
+        expect (isPalindrome ('racecar')).toBe(true);
+    })
+
+    it ("return false for 'car' ", () => {
+
+        expect (isPalindrome ('car')).toBe(false);
+    })
+
+    it ("return true for 'Mom' ", () => {
+
+        expect (isPalindrome ('Mom')).toBe(true);
+    })
+
+    it ("return false for an empty string ", () => {
+
+        expect (isPalindrome ('')).toBe(false);
+    });
+});
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
-// Your code here
+function isPalindrome (word) {
+
+    // conditional if to avaluate the constants
+
+    // return false fro  an empty string
+    if (word === '') return false;
+
+    const myWord = word.toLowerCase (); //return string to niormal case
+
+    const reversedword  = myWord.split ('').reverse().join ('') ;
+
+    return myWord === reversedword;
+
+}
+
+export default isPalindrome;
